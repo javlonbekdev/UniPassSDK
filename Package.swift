cd5cce6db315b43550b02fa1e859e43877521cc3
javlonbekdev <javlonbekdev@gmail.com>
2025-08-21 12:21:28 +0500

    swift-tools-version changed 6.1 to 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,17 +12,14 @@ let package = Package(
         .watchOS(.v8)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UniPassSDK",
             targets: ["UniPassSDK"]),
     ],
     dependencies: [
-            .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
-        ],
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1")
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "UniPassSDK",
             dependencies: ["SnapKit"]
